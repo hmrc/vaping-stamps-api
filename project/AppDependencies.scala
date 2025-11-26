711710d04cc6e9160a2da718f8c73182694ac0cc
hmrc-web-operations <hmrc-web-operations@digital.hmrc.gov.uk>
2025-11-26 17:20:41 +0000

    'Creating new service vaping-stamps-api'

---
+++ project/AppDependencies.scala
@@ -0,0 +1,20 @@
+import sbt.Keys.libraryDependencies
+import sbt._
+
+object AppDependencies {
+
+  private val bootstrapVersion = "10.4.0"
+  private val hmrcMongoVersion = "2.10.0"
+
+  val compile = Seq(
+    "uk.gov.hmrc"             %% "bootstrap-backend-play-30"  % bootstrapVersion,
+    "uk.gov.hmrc.mongo"       %% "hmrc-mongo-play-30"         % hmrcMongoVersion
+  )
+
+  val test = Seq(
+    "uk.gov.hmrc"             %% "bootstrap-test-play-30"     % bootstrapVersion            % Test,
+    "uk.gov.hmrc.mongo"       %% "hmrc-mongo-test-play-30"    % hmrcMongoVersion            % Test,
+  )
+
+  val it = Seq.empty
+}
