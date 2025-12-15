aa40b31de4c222e8384c1256f1ec3d321b8e1f16
vivekhmrc <vivek.kumar@digital.hmrc.gov.uk>
2025-12-15 09:07:33 +0000

    VSAP-11:Fixed the Service and the Unit tests for the Service to mock Mongo

--- test/uk/gov/hmrc/vapingstampsapi/service/HealthServiceSpec.scala
+++ test/uk/gov/hmrc/vapingstampsapi/service/HealthServiceSpec.scala
@@ -1,57 +1,49 @@
 package uk.gov.hmrc.vapingstampsapi.service
 
 import org.mockito.Mockito.*
+import org.scalatest.concurrent.ScalaFutures
 import org.scalatest.matchers.should.Matchers
 import org.scalatest.wordspec.AnyWordSpec
 import org.scalatestplus.mockito.MockitoSugar
 import uk.gov.hmrc.mongo.MongoComponent
-import scala.concurrent.Future
+import org.mongodb.scala.MongoDatabase
+import org.mockito.ArgumentMatchers.any
+import org.mongodb.scala.bson.Document
+
 import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.Future
 
-class HealthServiceSpec extends AnyWordSpec with Matchers with MockitoSugar {
+class HealthServiceSpec
+  extends AnyWordSpec
+    with Matchers
+    with MockitoSugar
+    with ScalaFutures {
 
-  "HealthService check" should {
+  "HealthService.check" should {
 
-    import org.mongodb.scala._
-    import org.mongodb.scala.bson.Document
-
-    "HealthService check should return true when Mongo responds to ping" in {
+    "return true when Mongo ping succeeds" in {
       val mongoComponent = mock[MongoComponent]
-      val database = mock[MongoDatabase]
-
-      val successObs = mock[org.mongodb.scala.SingleObservable[Document]]
-      when(successObs.toFuture()).thenReturn(Future.successful(Document()))
-
-      when(database.runCommand(Document("ping" -> 1))).thenReturn(successObs)
-
-      when(database.runCommand(Document("ping" -> 1)))
-        .thenReturn(Observable(Document()))
-
-
-      val service = new HealthService(mongoComponent)
-      service.check().map(_ shouldBe true)
-    }
-
-    "HealthService check should return false when Mongo ping fails" in {
-      val mongoComponent = mock[MongoComponent]
-      val database = mock[MongoDatabase]
-
-
-      val failedObs = mock[org.mongodb.scala.SingleObservable[Document]]
-      when(failedObs.toFuture()).thenReturn(Future.failed(new RuntimeException("boom")))
+      val database       = mock[MongoDatabase]
 
       when(mongoComponent.database).thenReturn(database)
-      when(database.runCommand(Document("ping" -> 1))).thenReturn(failedObs)
 
-      // success case
-      when(database.runCommand(Document("ping" -> 1)))
-        .thenReturn(Observable(Document()))
+      val service = spy(new HealthService(mongoComponent))
+      doReturn(Future.successful(()))
+        .when(service)
+        .pingMongo()
 
-
-      val service = new HealthService(mongoComponent)
-      service.check().map(_ shouldBe false)
+      whenReady(service.check())(_ shouldBe true)
     }
 
+    "return false when Mongo ping fails" in {
+      val mongoComponent = mock[MongoComponent]
+
+      val service = spy(new HealthService(mongoComponent))
+      doReturn(Future.failed(new RuntimeException("boom")))
+        .when(service)
+        .pingMongo()
+
+      whenReady(service.check())(_ shouldBe false)
+    }
   }
 }
-
