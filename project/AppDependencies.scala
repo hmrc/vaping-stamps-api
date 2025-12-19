8d7d026fc7a57e575cb40ba4ee9e85b120220bb6
pankaj2453 <12529426+pankaj2453@users.noreply.github.com>
2025-12-19 10:25:05 +0000

    VSAP-63 remove unused code and bum version

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -4,7 +4,7 @@ import sbt._
 object AppDependencies {
 
   private val bootstrapVersion = "10.4.0"
-  private val hmrcMongoVersion = "2.10.0"
+  private val hmrcMongoVersion = "2.11.0"
 
   val compile = Seq(
     "uk.gov.hmrc"             %% "bootstrap-backend-play-30"  % bootstrapVersion,
