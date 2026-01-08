be33265ebb3b26a9861dbfde792dd85c27f645ae
pankaj2453 <12529426+pankaj2453@users.noreply.github.com>
2026-01-08 13:03:33 +0000

    VSAP-113 remove mongo

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -4,16 +4,13 @@ import sbt._
 object AppDependencies {
 
   private val bootstrapVersion = "10.5.0"
-  private val hmrcMongoVersion = "2.11.0"
 
   val compile = Seq(
     "uk.gov.hmrc"             %% "bootstrap-backend-play-30"  % bootstrapVersion,
-    "uk.gov.hmrc.mongo"       %% "hmrc-mongo-play-30"         % hmrcMongoVersion
   )
 
   val test = Seq(
     "uk.gov.hmrc"             %% "bootstrap-test-play-30"     % bootstrapVersion            % Test,
-    "uk.gov.hmrc.mongo"       %% "hmrc-mongo-test-play-30"    % hmrcMongoVersion            % Test,
   )
 
   val it = Seq.empty
