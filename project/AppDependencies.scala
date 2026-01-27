9234bbb2903ad417d277252c110b648b97925e06
vivekhmrc <vivek.kumar@digital.hmrc.gov.uk>
2026-01-27 18:02:35 +0000

    VSAP-155: Added new scalafmt formatter. Formatted all the code using just added scalafmt formatter. Updated the README with the instructions to run

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -6,11 +6,11 @@ object AppDependencies {
   private val bootstrapVersion = "10.5.0"
 
   val compile = Seq(
-    "uk.gov.hmrc"             %% "bootstrap-backend-play-30"  % bootstrapVersion,
+    "uk.gov.hmrc" %% "bootstrap-backend-play-30" % bootstrapVersion
   )
 
   val test = Seq(
-    "uk.gov.hmrc"             %% "bootstrap-test-play-30"     % bootstrapVersion            % Test,
+    "uk.gov.hmrc" %% "bootstrap-test-play-30" % bootstrapVersion % Test
   )
 
   val it = Seq.empty
