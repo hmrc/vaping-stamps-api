a85cfef7bfe5d282cdfcfc76bb80632100c1c878
vivekhmrc <vivek.kumar@digital.hmrc.gov.uk>
2026-02-10 23:27:33 +0000

    VSAP-116: Create an end point to retrieve the purchasing operators' approval status.

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -1,16 +1,27 @@
-import sbt.Keys.libraryDependencies
 import sbt._
 
 object AppDependencies {
 
   private val bootstrapVersion = "10.5.0"
 
+  // Added: explicit test library versions (Scala 3 compatible)
+  private val scalaTestVersion = "3.2.18"
+  private val scalaTestPlusPlay = "7.0.1"
+  private val scalaTestPlusMockito = "3.2.18.0"
+  private val mockitoCoreVersion = "5.11.0"
+
   val compile = Seq(
     "uk.gov.hmrc" %% "bootstrap-backend-play-30" % bootstrapVersion
   )
 
   val test = Seq(
-    "uk.gov.hmrc" %% "bootstrap-test-play-30" % bootstrapVersion % Test
+    // HMRC bootstrap test bundle (Scala 3 safe)
+    // Includes ScalaTest, Play test helpers, Mockito core
+    "uk.gov.hmrc" %% "bootstrap-test-play-30" % bootstrapVersion % Test,
+    // ScalaTest support for integration tests
+    "org.scalatest" %% "scalatest" % "3.2.18" % Test,
+    // Play test helpers for integration tests
+    "org.scalatestplus.play" %% "scalatestplus-play" % "7.0.1" % Test
   )
 
   val it = Seq.empty
