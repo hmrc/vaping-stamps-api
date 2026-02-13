8f11f1aeb201776060f2388a946cc78e00e62dcc
vivekhmrc <vivek.kumar@digital.hmrc.gov.uk>
2026-02-13 11:03:52 +0000

    VSAP-116: Change to have APPROVED Status and additional test for full Payload verification

--- test/uk/gov/hmrc/vapingstampsapi/controllers/ApprovalControllerSpec.scala
+++ test/uk/gov/hmrc/vapingstampsapi/controllers/ApprovalControllerSpec.scala
@@ -47,5 +47,27 @@ class ApprovalControllerSpec extends AnyWordSpec with Matchers {
 
       status(result) mustBe OK
     }
+
+    "return full approval summary payload for a valid approval ID" in {
+      val approvalId = "AAAA0000200BB"
+
+      val request = FakeRequest(GET, s"/status/$approvalId/summary")
+      val result = controller.retrieveSummary(approvalId).apply(request)
+
+      status(result) mustBe OK
+      contentType(result) mustBe Some("application/json")
+
+      val json = contentAsJson(result)
+
+      (json \ "approvalStatus").as[String] mustBe "APPROVED"
+      (json \ "businessName").as[String] mustBe "Acme Vaping Ltd"
+      (json \ "registeredBusinessAddress").as[String] mustBe "1 Business Park, London, SW1A 1AA"
+      (json \ "correspondenceAddress").as[String] mustBe "PO Box 123, London, SW1A 2BB"
+      (json \ "contactName").as[String] mustBe "John Smith"
+      (json \ "contactTelephone").as[String] mustBe "02071234567"
+      (json \ "contactEmail").as[String] mustBe "john.smith@acmevaping.co.uk"
+      (json \ "approvalNumber").as[String] mustBe approvalId
+      (json \ "stampThreshold").as[Long] mustBe 10000L
+    }
   }
 }
