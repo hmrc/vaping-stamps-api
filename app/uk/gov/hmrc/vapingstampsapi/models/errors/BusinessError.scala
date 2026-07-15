94733d82994ddb312b10fcda7980934f2b6e078e
JSimmons072019 <53817037+JSimmons072019@users.noreply.github.com>
2026-07-15 10:08:03 +0100

    VSAP-292 Add '003' scenario for the 422 error (#76)
    * VSAP-292 Add '003' scenario for the 422 error
    
    # Conflicts:
    #	resources/public/api/conf/1.0/application.yaml
    
    * update wording
    
    * wip
    
    * adjusted naming
    
    * naming change

--- app/uk/gov/hmrc/vapingstampsapi/models/errors/BusinessError.scala
+++ app/uk/gov/hmrc/vapingstampsapi/models/errors/BusinessError.scala
@@ -17,7 +17,7 @@
 package uk.gov.hmrc.vapingstampsapi.models.errors
 
 import play.api.Logging
-import play.api.libs.json.{Format, JsError, JsString, JsSuccess, Reads, Writes}
+import play.api.libs.json.*
 
 trait BusinessError
 object BusinessError extends Logging:
@@ -25,6 +25,7 @@ object BusinessError extends Logging:
     Reads {
       case JsString("001") => JsSuccess(StampsReferenceNumberNotFound)
       case JsString("002") => JsSuccess(VdsEmailNotFound)
+      case JsString("003") => JsSuccess(MultipleStampsRequestReceived)
       case _               =>
         logger.error("[BusinessError][format]: Invalid BusinessError value")
         JsError("Invalid BusinessError value")
@@ -32,6 +33,7 @@ object BusinessError extends Logging:
     Writes {
       case StampsReferenceNumberNotFound => JsString(StampsReferenceNumberNotFound.toString)
       case VdsEmailNotFound              => JsString(VdsEmailNotFound.toString)
+      case MultipleStampsRequestReceived => JsString(MultipleStampsRequestReceived.toString)
     }
   )
 
@@ -40,3 +42,6 @@ case object StampsReferenceNumberNotFound extends BusinessError:
 
 case object VdsEmailNotFound extends BusinessError:
   override def toString: String = "002"
+
+case object MultipleStampsRequestReceived extends BusinessError:
+  override def toString: String = "003"
